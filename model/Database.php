b5bd28ec3e6c2b5c32a1b68347cb7296fa077a35
Kankor <waojedae@utn.edu.ec>
2018-12-10 12:25:13 -0500

    dfgsdf

--- model/Database.php
+++ model/Database.php
@@ -3,15 +3,16 @@
  * Clase utilitaria que maneja la conexion/desconexion a la base de datos
  * mediante las funciones PDO (PHP Data Objects).
  * Utiliza el patron de diseno singleton para el manejo de la conexion.
- * @author mrea
+* @author Mateo Salcedo
  */
 class Database {
 
     //Propiedades estaticas con la informacion de la conexion (DSN):
-    private static $dbName = 'ciclobdd';
-    private static $dbHost = 'localhost';
-    private static $dbUsername = 'root';
-    private static $dbUserPassword = '';
+    private static $dbName = 'dbbjk5jf60gd68';
+    private static $port = '5432';
+    private static $dbHost = 'ec2-107-21-125-209.compute-1.amazonaws.com';
+    private static $dbUsername = 'oriwjdontbpjfa';
+    private static $dbUserPassword = '1efde5034f466a3006e9c7943344251defe7224a0dd02d20cba320cecf24d6fd';
     //Propiedad para control de la conexion:
     private static $conexion = null;
 
@@ -31,21 +32,19 @@ class Database {
         // Una sola conexion para toda la aplicacion (singleton):
         if (null == self::$conexion) {
             try {
-                self::$conexion = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
+                self::$conexion = new PDO("pgsql:host=" . self::$dbHost . ";"."port=".self::$port .";". "dbname=" . self::$dbName, self::$dbUsername, self::$dbUserPassword);
             } catch (PDOException $e) {
                 die($e->getMessage());
             }
         }
         return self::$conexion;
     }
-
     /**
      * Metodo estatico para desconexion de la bdd.
      */
     public static function disconnect() {
         self::$conexion = null;
     }
-
 }
-
 ?>
+
